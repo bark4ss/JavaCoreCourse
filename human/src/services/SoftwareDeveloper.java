1fa9122769edd26ba4270438d019e83eadc7d0b7
bark4ss <v.telyatko@gmail.com>
2021-04-21 15:33:55 +0300

    Add examples new features of Java 9-15

---
+++ human/src/services/SoftwareDeveloper.java
@@ -0,0 +1,20 @@
+package services;
+
+import knowledgebase.Knowledge;
+
+public class SoftwareDeveloper {
+    private Knowledge knowledge;
+
+    public SoftwareDeveloper(Knowledge knowledge) {
+        this.knowledge = knowledge;
+    }
+
+    public String develop(String requirements) {
+        System.out.println(requirements);
+        System.out.println("Ну и требования...");
+
+        knowledge.obtain(requirements);
+
+        return "Вот такой вот софт";
+    }
+}
---
+++ knowledge-base/src/knowledgebase/Knowledge.java
@@ -0,0 +1,13 @@
+package knowledgebase;
+
+import knowledgebase.internal.KnowledgeHelper;
+
+public class Knowledge {
+    public String obtain(String requirements) {
+        String knowledge = KnowledgeHelper.obtainDomainKnowledge(requirements);
+        System.out.println("Теперь я всеееее знаю");
+        System.out.println(knowledge);
+
+        return knowledge;
+    }
+}
