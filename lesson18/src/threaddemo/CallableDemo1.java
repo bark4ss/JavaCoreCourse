3463af6ad6f646864a00fe54079b2926c077b36f
bark4ss <v.telyatko@gmail.com>
2021-04-30 12:52:25 +0300

    Update the lesson 18 - executorservice replaced by futuretask

--- lesson18/src/threaddemo/CallableDemo1.java
+++ lesson18/src/threaddemo/CallableDemo1.java
@@ -6,11 +6,10 @@ import java.util.concurrent.*;
 
 public class CallableDemo1 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
         FactorialTask task = new FactorialTask(5);
-        Future<Integer> future = executorService.submit(task);
-        executorService.shutdown();
-        System.out.println(future.get().intValue());
+        FutureTask<Integer> futureTask = new FutureTask<>(task);
+        new Thread(futureTask).start();
+        System.out.println(futureTask.get().intValue());
 
     }
 }
